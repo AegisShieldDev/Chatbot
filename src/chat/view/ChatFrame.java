affb0cbfb383059f7ab8826de96eece6bba6d5d8
Outrider99 <outrider.w@gmail.com>
2017-11-14 11:53:28 -0700

    Added ChatFrame and ChatPanel

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -3,7 +3,24 @@ package chat.view;
 import chat.controller.ChatController;
 import javax.swing.JFrame;
 
-public class ChatFrame
+public class ChatFrame extends JFrame
 {
-
+	public ChatController appController;
+	
+	public ChatFrame(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setSize(600,600);
+		this.setTitle("Chatting with the chatbot");
+		this.setResizable(false);
+		this.setContentPane(appPane);
+		this.setVisible(true);
+	}
 }
---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,36 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import chat.controller.ChatController;
+
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+
+public class ChatPanel
+{
+	private ChatController appController;
+	
+	public ChatPanel(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	private void setupLayout()
+	{
+		
+	}
+	private void setupListeners()
+	{
+	
+	}
+}
