64f821bd2112d3ea53ed50d54b15bd407eaeda2a
Outrider99 <outrider.w@gmail.com>
2018-03-01 12:32:14 -0700

    added getmostcommonword, turnstatusestowords, and removepunctuation

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -12,7 +12,7 @@ public class CTECTwitter
 {
 	private ChatbotController appController;
 	private Twitter chatbotTwitter;
-	private List<Status> searchedTwitter;
+	private List<Status> searchedTweets;
 	private List<String> tweetedWords;
 	private long totalWordCount;
 	
@@ -20,9 +20,8 @@ public class CTECTwitter
 	{
 		this.appController = appController;
 		this.chatbotTwitter = TwitterFactory.getSingleton();
-		this.searchedTwitter = new ArrayList<Status>();
+		this.searchedTweets = new ArrayList<Status>();
 		this.tweetedWords = new ArrayList<String>();
-		this.chatbotTwitter = TwitterFactory.getSingleton();
 		this.totalWordCount = 0;
 		
 	}
@@ -41,5 +40,75 @@ public class CTECTwitter
 		{
 			appController.handleErrors(otherError);
 		}
+	}
+	
+	public String getMostCommonWord(String username)
+	{
+		String mostCommon = "";
+		
+		collectTweets(username);
+		
+		return mostCommon;
+		
+	}
+	
+	private void collectTweets(String username)
+	{
+		searchedTweets.clear();
+		tweetedWords.clear();
+		
+		Paging statusPage = new Paging(1,100);
+		int page = 1;
+		long lastID = Long.MAX_VALUE;
+		
+		while (page <= 10)
+		{
+			statusPage.setPage(page);
+			try
+			{
+				ResponseList<Status> listedTweets = chatbotTwitter.getUserTimeline(username, statusPage);
+				for(Status current : listedTweets)
+				{
+					if(current.getId() < lastID)
+					{
+						searchedTweets.add(current);
+						lastID = current.getId();
+					}
+				}
+			}
+			catch(TwitterException searchTweetError)
+			{
+				appController.handleErrors(searchTweetError);
+			}
+			page++;
+		}
+	}
+	
+	private void turnStatusesToWords()
+	{
+		for(Status currentStatus : searchedTweets)
+		{
+			String tweetText = currentStatus.getText();
+			String [] tweetWords = tweetText.split(" ");
+			for(int index = 0; index < tweetWords.length; index++)
+			{
+				tweetedWords.add(removePunctuation(tweetWords[index]).trim());
+			}
+		}
+	}
+	
+	private String removePunctuation(String currentString)
+	{
+		String punctuation = ".,'?!:;\"() {}^[]<>-";
+		
+		String scrubbedString = "";
+		for(int i = 0; i < currentString.length(); i++)
+		{
+			if(punctuation.indexOf(currentString.charAt(i)) == -1)
+			{
+				scrubbedString += currentString.charAt(i);
+			}
+		}
+		return scrubbedString;
 	}
 }
